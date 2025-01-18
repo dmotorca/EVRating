d543a5444bd408996d1054cce1bbade261af5bcd
David Motorca <dmotorca@gmail.com>
2025-01-18 12:46:06 -0800

    Changing a few things and adding env file to Vercel to use DB on production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,113 @@
-//import Selector from '@/components/Selector';
-//import Footer from '../components/Footer';
+import React from 'react';
+import Selector from '@/components/Selector';
 
-export default function Home() {
+const VehicleSelectorPage = () => {
+  const makes = [
+    'AM General',
+    'Acura',
+    'Alfa Romeo',
+    'American Motors Corporation',
+    'Audi',
+    'BMW',
+    'Buick',
+    'Cadillac',
+    'Chevrolet',
+    'Chrysler',
+    'CX Automotive',
+    'Dodge',
+    'E. P. Dutton, Inc.',
+    'Eagle',
+    'Ferrari',
+    'Ford',
+    'Genesis',
+    'Geo',
+    'GMC',
+    'Grumman Olson',
+    'Honda',
+    'Hyundai',
+    'Infiniti',
+    'Isuzu',
+    'Jaguar',
+    'Jeep',
+    'Kia',
+    'Land Rover',
+    'Lexus',
+    'Lincoln',
+    'Lotus',
+    'Lucid Motors',
+    'Mazda',
+    'Mercedes-Benz',
+    'Mercury',
+    'Mitsubishi',
+    'Nissan',
+    'Oldsmobile',
+    'PAS, Inc',
+    'Plymouth',
+    'Polestar',
+    'Pontiac',
+    'Porsche',
+    'Ram',
+    'Rolls-Royce',
+    'Saturn',
+    'Subaru',
+    'Suzuki',
+    'Tesla',
+    'Toyota',
+    'Volkswagen',
+    'Volvo',
+    'Rivian',
+  ];
+
+  const years = [
+    '1984',
+    '1985',
+    '1986',
+    '1987',
+    '1988',
+    '1989',
+    '1990',
+    '1991',
+    '1992',
+    '1993',
+    '1994',
+    '1995',
+    '1996',
+    '1997',
+    '1998',
+    '1999',
+    '2000',
+    '2001',
+    '2002',
+    '2003',
+    '2004',
+    '2005',
+    '2006',
+    '2007',
+    '2008',
+    '2009',
+    '2010',
+    '2011',
+    '2012',
+    '2013',
+    '2014',
+    '2015',
+    '2016',
+    '2017',
+    '2018',
+    '2019',
+    '2020',
+    '2021',
+    '2022',
+    '2023',
+    '2024',
+    '2025',
+  ];
+
   return (
-    <div>
-      <h1>MAIN PAGE</h1>
+    <div className="flex flex-col lg:flex-row lg:gap-4">
+      <Selector optionsYears={years} optionsMakes={makes} />
     </div>
   );
-}
+};
+
+export default VehicleSelectorPage;
